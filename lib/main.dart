e8336e29e370be3b98b29c9797e88be88f86949f
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2025-11-22 15:03:06 -0700

    Begin layout home page

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hp Tracker',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Color.fromARGB(255, 124, 7, 170)),
       ),
       darkTheme: ThemeData(
@@ -38,7 +23,15 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 124, 7, 170), brightness: Brightness.dark)
       ),
       themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(
+        title: 'Hp Tracker',
+        characterName: "Charles",
+        currentHp: 25,
+        maxHp: 25,
+        currentTempHp: 0,
+        maxTempHp: 0,
+        currentHitDice: 10,
+        maxHitDice: 10,),
     );
   }
 }
--- lib/ui/home/widgets/home_screen.dart
+++ lib/ui/home/widgets/home_screen.dart
@@ -1,32 +1,63 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({
+    super.key, 
+    required this.title,
+    required this.characterName,
+    required this.currentHp,
+    required this.maxHp,
+    required this.maxTempHp,
+    required this.currentTempHp,
+    required this.currentHitDice,
+    required this.maxHitDice});
   final String title;
+
+  final String characterName;
+  final int currentHp;
+  final int maxHp;
+  final int currentTempHp;
+  final int maxTempHp;
+  final int currentHitDice;
+  final int maxHitDice;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hp Tracker'),
+        title: Center(child: Text(widget.title.toUpperCase()),),
         elevation: 2,
       ),
-      body: ElevatedButton(child: Text("Welcome Home!"), onPressed: () => print("button clicked!"),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              children: [
+              Text(widget.characterName),
+            ],),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Text("HP: ${widget.currentHp} / ${widget.maxHp}")
+                  ],
+                ),
+                Column(
+
+                )
+              ],
+            )
+
+        ],
+        ),
+      ),
       drawer: Column(
         children: [Placeholder()],
       ),
